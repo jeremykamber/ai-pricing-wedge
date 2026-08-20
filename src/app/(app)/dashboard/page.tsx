1a1d8194d573c3c41b82d142d4e5da134172c537
Jeremy Kamber <jkamberwork@gmail.com>
2026-08-20 19:19:35 +0200

    chore: remove temporary test dismiss button

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import { DashboardClient } from '@/ui/dashboard/components/DashboardClient'
-import { TestDismissButton } from '@/components/custom/TestDismissButton'
 
 export default function DashboardPage() {
   return (
     <div className="flex flex-col gap-8 w-full h-full">
       <DashboardClient />
-      <TestDismissButton />
     </div>
   );
 }
